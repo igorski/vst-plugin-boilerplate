78417176f8f7eaf4b77f7f17e3aacd8af51c3afa
Igor Zinken <igor.zinken@igorski.nl>
2023-01-12 12:56:05 +0100

    Updated Windows 32-bit build scripts

--- build32.bat
+++ build32.bat
@@ -3,9 +3,9 @@ rmdir /Q /S build
 mkdir build
 cd build
 
-cmake -G "Visual Studio 16 2019" -A Win32 -S .. -B "build32" -DSMTG_CREATE_VST2_VERSION=ON -DVST3_SDK_ROOT=%VST3_SDK_ROOT_32BIT%
-cmake --build build32 --config Release
+cmake -G "Visual Studio 16 2019" -A Win32 -S .. -DSMTG_CREATE_VST2_VERSION=ON -DVST3_SDK_ROOT=%VST3_SDK_ROOT_32BIT%
+cmake --build . --config Release
 
-rename build32\VST3\Release\__PLUGIN_NAME__.vst3\Contents\x86-win\__PLUGIN_NAME__.vst3 __PLUGIN_NAME__.dll
+rename VST3\Release\__PLUGIN_NAME__.vst3\Contents\x86-win\__PLUGIN_NAME__.vst3 __PLUGIN_NAME__.dll
 
 cd ..
