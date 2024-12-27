f32fe7c5a29ee8ebb8725c9d0b3005d0a26bc3f1
Igor Zinken <igor.zinken@gmail.com>
2024-12-27 07:58:26 +0100

    Updated Windows 32-bit build batch file

--- build32.bat
+++ build32.bat
@@ -12,6 +12,6 @@ echo Building using VST3 SDK located at %VST3_SDK_ROOT%
 cmake -G "Visual Studio 16 2019" -A Win32 -S .. -B "build32" -DSMTG_CREATE_VST2_VERSION=ON -DVST3_SDK_ROOT=%VST3_SDK_ROOT_32BIT%
 cmake --build build32 --config Release
 
-rename build32\VST3\Release\__PLUGIN__.vst3\Contents\x86-win\__PLUGIN__.vst3 __PLUGIN__.dll
+rename build32\VST3\Release\__PLUGIN_NAME__.vst3\Contents\x86-win\__PLUGIN_NAME__.vst3 __PLUGIN_NAME__.dll
 
 cd ..
